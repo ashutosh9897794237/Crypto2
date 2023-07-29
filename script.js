fa6d1fe1907f3a7fe7a8f86df7273ee5f98cbed7
ashutosh9897794237 <113291959+ashutosh9897794237@users.noreply.github.com>
2023-07-29 23:45:10 +0530

    hl

--- script.js
+++ script.js
@@ -15,7 +15,7 @@
 // }).catch((err)=>{
 //     console.log(err);
 // })
-const url = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false";
+const url = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&locale=en";
 async function getData(url) {
     console.log("functionCalled")
     const response = await fetch(url);
